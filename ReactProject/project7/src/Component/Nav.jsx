5d1405ecb06babec6348a20d1782f0e0a356dd3f
ghosounalrahahleh <ghosoun.alrahahleh@gmail.com>
2022-01-28 20:25:28 +0200

    login and register

---
+++ ReactProject/project7/src/Component/Nav.jsx
@@ -0,0 +1,83 @@
+
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export class Nav extends Component {
+  render() {
+    return (
+      <header className="site-navbar" role="banner">
+        <div className="site-navbar-top" style={{ height: "5vh" }}>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
+                <form action="" className="site-block-top-search">
+                  <span className="icon icon-search2"></span>
+                  <input
+                    type="text"
+                    className="form-control border-0"
+                    placeholder="Search"
+                  />
+                </form>
+              </div>
+
+              <div className="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
+                <div className="site-logo" style={{ marginTop: -20 }}>
+                  <a href="index.html" className="js-logo-clone">
+                    <img src="/images/logo.jpg" alt="" width="100px" />
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-6 col-md-4 order-3 order-md-3 text-right">
+                <div className="site-top-icons">
+                  <ul>
+                    <li>
+                      <NavLink to="/profile">
+                        <span className="icon icon-person"></span>
+                      </NavLink>
+                    </li>
+
+                    <li>
+                      <NavLink to="/cart" className="site-cart">
+                        <span className="icon icon-shopping_cart"></span>
+                        <span className="count">2</span>
+                      </NavLink>
+                    </li>
+                    <li className="d-inline-block d-md-none ml-md-0">
+                      <a href="#id" className="site-menu-toggle js-menu-toggle">
+                        <span className="icon-menu"></span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <nav
+          className="site-navigation text-right text-md-center"
+          role="navigation"
+        >
+          <ul className="site-menu js-clone-nav d-none d-md-block">
+            <li >
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li >
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/shop">Shop</NavLink>
+            </li>    
+            <li>
+              <NavLink to="/register">Register</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
