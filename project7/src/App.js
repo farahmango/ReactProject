238193f460062cd94126edb1fb47131ea09c6be2
osaid ghnaimat <osaid.ghnaimat@gmail.com>
2022-01-29 16:40:55 +0200

    test2

--- project7/src/App.js
+++ project7/src/App.js
@@ -9,8 +9,24 @@ import About from "./Component/About.jsx";
 import {Body} from "./Component/Body";
 import Profile from "./Component/Profile.jsx";
 import {Header} from "./Component/Header";
+// import Data from "./Component/data";
+import React,{useState,useEffect} from "react";
+
+
 
 function App() {
+
+    const [item, setItem] = useState([]);
+    useEffect(()=>{
+      fetch("http://127.0.0.1/ReactProject/project7/src/Component/shop.php")
+      .then(res => res.json())
+      .then(
+        (result)=>{
+          setItem(result);
+        }
+      )
+    }, []);
+    
   return (
     <BrowserRouter>
       <div className="site-wrap">
@@ -19,12 +35,13 @@ function App() {
         <Route  path="/" element={<Body />} />
           <Route path="/register" element={<Register />} />
           <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop" element={<Shop item={item}/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
 
+        {/* <Data /> */}
         <Footer />
       </div>
     </BrowserRouter>
---
+++ project7/src/Component/data.js
@@ -0,0 +1,22 @@
+// import React,{useState,useEffect} from "react";
+
+// const Data =() => {
+// 	const [item, setItem] = useState([]);
+// 	useEffect(()=>{
+// 		fetch("http://127.0.0.1/ReactProject/project7/src/Component/shop.php")
+// 		.then(res => res.json())
+// 		.then(
+// 			(result)=>{
+// 				setItem(result);
+// 				// console.log(result);
+// 			}
+// 		)
+// 	}, []);
+// 		return (
+// 			<div className="d_flex my-4" >
+				
+// 			</div>
+// 		);
+// }
+ 
+// export default Data ;
